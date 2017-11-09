1e5cec4520f8106b8bdbb9afbea6e10110335544
Gavin Eke <gavineke-github@yahoo.com.au>
2017-11-09 21:22:01 +1100

    Remove old comment and update compatible versions

--- T.DEV/T.DEV.psm1
+++ T.DEV/T.DEV.psm1
@@ -1,10 +1,4 @@
-﻿<#
-    This PowerShell module uses the Cloudflare® API to manage Cloudflare services.
-    This PowerShell module is a community project and not officially supported by Cloudflare.
-    Make sure to read Cloudflare's Terms of Use - https://www.cloudflare.com/terms/
-#>
-
-# Set PSScriptRoot
+﻿# Set PSScriptRoot
 If (-not ($PSScriptRoot)) {
     $PSScriptRoot = Split-Path $MyInvocation.MyCommand.Path -Parent
 }
